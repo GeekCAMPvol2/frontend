69f8e65ad4e9e227a63cfc119cd0ccc425c73abd
yoshi-non <himekawa.yoshino44@gmail.com>
2023-04-07 19:05:43 +0900

    lobby.tsxのHomeButton

---
+++ src/pages/lobby.tsx
@@ -0,0 +1,14 @@
+import HomeButton from '@/components/elements/HomeButton';
+
+const Lobby = () => {
+  return (
+    <div>
+      {/* ホームボタンコンポーネント */}
+      <div>
+        <HomeButton />
+      </div>
+    </div>
+  );
+};
+
+export default Lobby;
