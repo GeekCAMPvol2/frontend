9cea5370f6b9cea3c762c301f9092fc12d5d112e
NewNomad <tosiki1997@gmail.com>
2023-04-09 14:18:36 +0900

    feat: hanbdleEvents

--- src/components/game/KeyPadCard.tsx
+++ src/components/game/KeyPadCard.tsx
@@ -1,11 +1,38 @@
-const KeyPadCard = () => {
-  return (
-    <div>
-      <input type="number" />
+import { Styles } from "@/types/Styles";
+import { NumButton } from "./NumButton";
+import { ChangeEvent, useState } from "react";
 
+const KeyPadCard = () => {
+  const [input, setinput] = useState()
+
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+
+  }
+
+  const handleNumButton = () => {
+
+  }
+
+  return (
+    <div style={styles.container}>
+      <input type="number" value={input} onChange={handleInput} />
+      <div style={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr 1fr"
+      }}>
+        <NumButton number={1} onClick={handleNumButton} />
+
+      </div>
 
     </div>
   );
 };
+
+const styles: Styles = {
+  container: {
+    textAlign: "center",
+  }
+}
 
 export default KeyPadCard;
--- src/components/game/NumButton.tsx
+++ src/components/game/NumButton.tsx
@@ -1,13 +1,41 @@
+import { Styles } from '@/types/Styles'
 import React from 'react'
+import { motion } from "framer-motion"
 
 type Props = {
     number: number
+    onClick: () => void
 }
 
 export const NumButton = (props: Props) => {
-    const { number } = props
+    const { number, onClick } = props
 
     return (
-        <button>{number}</button>
+        <motion.div
+            whileTap={{
+                scale: 0.9
+            }}
+        >
+            <button
+                style={styles.button}
+                onClick={onClick}
+            >
+                {number}
+            </button>
+        </motion.div>
     )
+}
+
+const styles: Styles = {
+    button: {
+        width: 55,
+        height: 55,
+        fontSize: 30,
+        color: "#000",
+        margin: "0 auto",
+        marginBottom: 10,
+        borderRadius: "50%"
+
+    }
+
 }
