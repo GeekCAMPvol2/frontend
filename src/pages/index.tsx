8ca8148846f2da4b38db9a1bc144e06dd94b4583
NewNomad <tosiki1997@gmail.com>
2023-04-09 18:13:08 +0900

    fix: title name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,8 @@ export default function Home() {
   return (
     <div>
       <main style={styles.main}>
-        <h1>タイトル</h1>
+        <h1>Price<br />Quest</h1>
+        <h3>〜失われた金銭感覚を求めて〜</h3>
         <div style={styles.buttonContainer}>
           <MainButton name="スタート" />
           <MainButton name="一人で遊ぶ" />
@@ -25,6 +26,7 @@ const styles: Styles = {
   main: {
     width: 1000,
     margin: "0 auto",
+    marginTop: 100,
     textAlign: "center"
   },
   buttonContainer: {
