795620bd22645206eeaa169979d2cdeedbcbcb93
NewNomad <tosiki1997@gmail.com>
2023-04-09 13:42:08 +0900

    fix: import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
-import TestChild from '@/components/TestChild';
 import { MainButton } from '@/components/index/MainButton';
-import { textState } from '@/store/atoms';
-import styles from '@/styles/Home.module.css';
-import { useRecoilState } from 'recoil';
 
 export default function Home() {
 
 
   return (
     <div>
-      <main>
+      <main >
         <h1>タイトル</h1>
         <MainButton name="スタート" />
         <MainButton name="一人で遊ぶ" />
