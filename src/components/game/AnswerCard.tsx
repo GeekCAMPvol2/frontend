38406c80dda0c016fc18ad8ff997e0977185d7b7
yoshi-non <himekawa.yoshino44@gmail.com>
2023-04-13 04:07:38 +0900

    予想金額を解答画面に渡す

--- src/components/game/AnswerCard.tsx
+++ src/components/game/AnswerCard.tsx
@@ -1,17 +1,19 @@
-import { itemData } from '@/store/atoms';
+import { itemData, keyPadNumState } from '@/store/atoms';
 import { useRecoilState } from 'recoil';
 
 const AnswerCard = () => {
   const [item, setItem] = useRecoilState(itemData);
+  const [keyPadNum, setKeyPadNum] =
+    useRecoilState(keyPadNumState);
 
   return (
     <div>
       <p>正解</p>
-      <h1>10000円</h1>
+      <h1>{keyPadNum}</h1>
       <p>あなたの回答</p>
       <h1>{item.answer}円</h1>
       <p>差額</p>
-      <h1>{10000 - item.answer}円</h1>
+      <h1>{keyPadNum - item.answer}円</h1>
     </div>
   );
 };
--- src/components/game/KeyPadCard.tsx
+++ src/components/game/KeyPadCard.tsx
@@ -1,6 +1,8 @@
-import { Styles } from "@/types/Styles";
-import { NumButton } from "./NumButton";
-import { ChangeEvent, useCallback, useState } from "react";
+import { Styles } from '@/types/Styles';
+import { NumButton } from './NumButton';
+import { ChangeEvent, useCallback } from 'react';
+import { useRecoilState } from 'recoil';
+import { keyPadNumState } from '@/store/atoms';
 
 // type Props = {
 //   setNumber: () => void
@@ -9,68 +11,83 @@ import { ChangeEvent, useCallback, useState } from "react";
 const KeyPadCard = () => {
   // const { setNumber } = props
 
-  const [input, setinput] = useState<number>(0)
+  const [keyPadNum, setKeyPadNum] =
+    useRecoilState(keyPadNumState);
 
   // 数字を入れる共通の関数
   const functionInsertNumber = (number: number) => {
-    number = Math.max(number, 0)
+    number = Math.max(number, 0);
     // number = Math.min(number, Infinity)
-    setinput(number)
+    setKeyPadNum(number);
 
     // ### 親にinput渡すときは、ここに記載してください
-  }
+  };
 
   // 直接数字を入れる関数
-  const handleInput = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    let number = parseInt(e.target.value)
-    number = Math.max(number, 0)
-    setinput(number)
-  }, [input])
+  const handleInput = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      let number = parseInt(e.target.value);
+      number = Math.max(number, 0);
+      setKeyPadNum(number);
+    },
+    [keyPadNum]
+  );
 
   // キーパッドから数字を入れる関数
   const handleNumButton = (e: number) => {
-    const number = input! * 10 + e
-    functionInsertNumber(number)
-  }
+    const number = keyPadNum! * 10 + e;
+    functionInsertNumber(number);
+  };
 
   const handleDelete = () => {
-    const number = Math.floor(input / 10)
-    setinput(number)
-  }
+    const number = Math.floor(keyPadNum / 10);
+    setKeyPadNum(number);
+  };
 
   return (
     <div style={styles.container}>
-      <input type="number" value={input} onChange={handleInput} style={styles.input} />
-      <div style={{
-        display: "grid",
-        gridTemplateColumns: "1fr 1fr 1fr"
-      }}>
+      <input
+        type="number"
+        value={keyPadNum}
+        onChange={handleInput}
+        style={styles.input}
+      />
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr 1fr',
+        }}
+      >
         {[...Array(9)].map((e, i) => (
-          <NumButton number={i + 1} onClick={() => handleNumButton(i + 1)} />
+          <NumButton
+            number={i + 1}
+            onClick={() => handleNumButton(i + 1)}
+          />
         ))}
-        <NumButton number={"del"} onClick={handleDelete} />
-        <NumButton number={0} onClick={() => handleNumButton(0)} />
+        <NumButton number={'del'} onClick={handleDelete} />
+        <NumButton
+          number={0}
+          onClick={() => handleNumButton(0)}
+        />
         {/* <NumButton number={i + 1} onClick={() => handleNumButton(i + 1)} /> */}
-
       </div>
-
     </div>
   );
 };
 
 const styles: Styles = {
   container: {
-    textAlign: "center",
+    textAlign: 'center',
   },
   input: {
-    width: "80%",
+    width: '80%',
     fontSize: 30,
-    textAlign: "left",
+    textAlign: 'left',
     padding: 10,
-    paddingBottom: 2  ,
+    paddingBottom: 2,
     marginBottom: 15,
-    borderBottom: "1px solid #000"
-  }
-}
+    borderBottom: '1px solid #000',
+  },
+};
 
 export default KeyPadCard;
