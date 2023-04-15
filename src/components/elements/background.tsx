8147c750933e881244bac75971052e454868c9c6
NewNomad <tosiki1997@gmail.com>
2023-04-15 22:55:04 +0900

    feat: background.tsx

---
+++ src/components/elements/background.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export const background = () => {
+    return (
+        <>
+            <div className="background">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </>
+    )
+}
