6a726b3e9d50b5aebb6d4851fe2f60f1395f3ec6
yoshi-non <himekawa.yoshino44@gmail.com>
2023-06-05 19:08:21 +0900

    MultiAnsの作成

--- src/components/multi/flgComponents/MultiAns.tsx
+++ src/components/multi/flgComponents/MultiAns.tsx
@@ -1,40 +1,75 @@
-import { Styles } from '@/types/Styles';
+import { css } from '@emotion/react';
+import Image from 'next/image';
+import AnsQuizButton from '../AnsQuizButton';
+import AnswerCard from '../AnswerCard';
+
+const styles = {
+  container: css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+  `,
+  leftContainer: css`
+    flex: 3;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 50px;
+  `,
+  productTitleWrapper: css``,
+  productTitle: css``,
+
+  productImageWrapper: css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+  `,
+  productImage: css`
+    width: 100%;
+    height: 500px;
+    object-fit: contain;
+  `,
+
+  rightContainer: css`
+    flex: 2;
+  `,
+};
 
 const MultiAns = () => {
-  return <div>Ans</div>;
+  return (
+    <div css={styles.container}>
+      {/* 左側 */}
+      <div css={styles.leftContainer}>
+        <div css={styles.productTitleWrapper}>
+          {/* 商品名 */}
+          <h2>Name</h2>
+          <h2 css={styles.productTitle}>
+            商品名商品名商品名商品名商品名商品名商品名商品名商品名商品名商品名商品名商品名商品名商品名商品名商品名
+          </h2>
+          <h2>9,999,999円</h2>
+        </div>
+        <div css={styles.productImageWrapper}>
+          {/* 商品画像 */}
+          <Image
+            css={styles.productImage}
+            src={'/flower2.jpg'}
+            alt={'テスト画像'}
+            width={100}
+            height={100}
+          />
+        </div>
+      </div>
+      {/* 右側 */}
+      <div css={styles.rightContainer}>
+        <AnswerCard />
+        <AnsQuizButton />
+      </div>
+    </div>
+  );
 };
 
 export default MultiAns;
-
-const styles: Styles = {
-  container: {
-    margin: '50px 0',
-  },
-  titleWrapper: {
-    textAlign: 'center',
-  },
-  wrapper: {
-    width: '1200px',
-    margin: '0 auto',
-    display: 'flex',
-    gap: '50px',
-  },
-  leftWrapper: {
-    flex: 3,
-    textAlign: 'center',
-  },
-  itemImageWrapper: {
-    border: '2px solid black',
-    borderRadius: '10px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  rightWrapper: {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    gap: '50px',
-  },
-};
